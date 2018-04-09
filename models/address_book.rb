95bb42e873b6d2bb085bde79fcac09c0cb961fdc
EC2 Default User <ec2-user@ip-172-31-87-66.ec2.internal>
2018-04-09 21:03:08 +0000

    remove entry assignment

--- models/address_book.rb
+++ models/address_book.rb
@@ -20,5 +20,15 @@ class AddressBook
         
         entries.insert(index, Entry.new(name, phone_number, email))
         
-    end 
+    end
+    
+    def remove_entry (name, phone_number, email)
+    
+        entries.each_with_index do |entry, index|
+            if name === entry.name
+                entries.delete_at(index)
+                 
+            end
+        end    
+    end    
 end    
